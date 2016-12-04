81a2bd916082bee205661aa090794b76df77449f
Peter Jang <peter.x.jang@gmail.com>
2016-12-04 11:19:23 -0600

    Add some validations to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,10 @@
 class Contact < ApplicationRecord
   belongs_to :user
+
+  validates :email, presence: true
+  validates :email, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
   
   def friendly_updated_at
     updated_at.strftime("%B %e, %Y")
